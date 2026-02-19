ac2cd865e6557ba89b3e9ff6fd1a68d88a9ff8b8
rknairporttaxi-commits <rknairporttaxi@gmail.com>
2026-02-19 22:59:57 +0530

    newwww

--- client/src/assets/frontend_assets/assets.js
+++ client/src/assets/frontend_assets/assets.js
@@ -56,7 +56,7 @@ import p_img52 from './p_img52.png'
 
 
 //import logo from './logo.png'
-import logo from "./jack_logo_1.png"
+import logo from "./jack_logo_2.png"
 import hero_img from './hero_img.png'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
